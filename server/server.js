bd6b544bda0514ba8ba11c5bddb0a969c4ed499b
Kushal Dutta <142610472+Kushal2025@users.noreply.github.com>
2025-09-15 23:06:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const cors = require('cors');
+const connectDB = require('../config/dD');
+const taskRoutes = require('../routes/taskRoutes');
+
 const app = express();
-const connectDB = require('./config/dD');
-const taskRoutes = require('./routes/taskRoutes');
-const bodyParser = require('body-parser');
-dotenv.config();
 
-
-// connectDB();
-connectDB(); // connect to MongoDB
+// Middlewares
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
-app.use('/tasks',taskRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Routes
+app.use('/tasks', taskRoutes);
+
+// Connect to MongoDB
+connectDB();
+
+// Export the app as a Vercel serverless function
+module.exports = app;
